56472e614fb93fdb299a5f0710cd989404b71dc4
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2020-04-04 18:51:36 +0200

    add MIT license

--- Client/Ftp/Behavior/FtpClientBehaviorFactoryRegistry.php
+++ Client/Ftp/Behavior/FtpClientBehaviorFactoryRegistry.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @package    3slab/VdmLibraryBundle
+ * @copyright  2020 Suez Smart Solutions 3S.lab
+ * @license    https://github.com/3slab/VdmLibraryBundle/blob/master/LICENSE
+ */
+
 namespace Vdm\Bundle\LibraryBundle\Client\Ftp\Behavior;
 
 use Psr\Log\LoggerInterface;
